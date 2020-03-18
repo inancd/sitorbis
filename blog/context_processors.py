09038f79c558820336a1d355365b35ddb9ae6693
İnanç Dağyolu <inanc@sitorbis.com>
2020-03-19 01:34:36 +0300

    v1.1.4i

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from blog.models import Post
+from blog.models import Post, PostCategory
 
 def blogs(request):
-    publishing_blog = Post.objects.all()
+    publishing_blog = PostCategory.objects.all()
 
     return {'blogs': publishing_blog}
